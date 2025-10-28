acc3058f5fd69155ab32f9b43b77e2033b213d61
StefanNeuberger <stef.neuberger@gmail.com>
2025-10-28 21:03:16 +0100

    add dependency to ShopService

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -3,9 +3,8 @@ import model.OrderStatus;
 import model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import repository.OrderMapRepo;
-import repository.OrderRepo;
-import repository.ProductRepo;
+import repository.*;
+import service.ShopService;
 
 import java.util.List;
 
@@ -19,8 +18,9 @@ class ShopServiceTest {
     void setUp() {
         ProductRepo productRepo = new ProductRepo();
         OrderRepo orderRepo = new OrderMapRepo();
+        IdGeneratorRepository idGeneratorRepository = new StringIdGeneratorRepo();
         productRepo.addProduct(new Product("1", "Apfel"));
-        shopService = new ShopService(productRepo, orderRepo);
+        shopService = new ShopService(productRepo, orderRepo, idGeneratorRepository);
 
     }
 
