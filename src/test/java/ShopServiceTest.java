ecb4957440732272088729f6f3a4957c050e66e7
StefanNeuberger <stef.neuberger@gmail.com>
2025-10-29 11:16:40 +0100

    testing processCommandsFromFile flow

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import repository.*;
 import service.ShopService;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
@@ -161,7 +164,246 @@ class ShopServiceTest {
                 assertNull(oldestOrderPerStatus.get(orderStatus));
             }
         }
+    }
 
+    @Test
+    void processCommandsFromFile_shouldThrowIOException_whenFileDoesNotExist() throws Exception {
+        Path nonExistentFile = Path.of("nonExistentFile");
+        assertThrows(IOException.class, () -> shopService.processCommandsFromFile(nonExistentFile));
+    }
+
+
+    @Test
+    void processCommandsFromFile_shouldProcessSuccessfully_whenFileIsEmpty() throws IOException {
+        //GIVEN
+        // Create temporary empty file
+        Path emptyFile = Files.createTempFile("test-empty", ".txt");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(emptyFile);
+
+            //THEN
+            // Verify no orders were processed (service should still work)
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(0, allOrders.size(), "No orders should be created from empty file");
+        } finally {
+            Files.deleteIfExists(emptyFile); // Clean up
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldCreateOrder_whenAddOrderCommandIsValid() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-addOrder", ".txt");
+        Files.writeString(testFile, "addOrder A 1");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(1, allOrders.size());
+            assertEquals(OrderStatus.PROCESSING, allOrders.get(0).orderStatus());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldCreateMultipleOrders_whenMultipleAddOrderCommands() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-multiple", ".txt");
+        Files.writeString(testFile, "addOrder A 1\naddOrder B 1");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(2, allOrders.size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldUpdateOrderStatus_whenSetStatusCommandIsValid() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-setStatus", ".txt");
+        Files.writeString(testFile, "addOrder A 1\nsetStatus A COMPLETED");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> completedOrders = shopService.getOrdersByOrderStatus(OrderStatus.COMPLETED);
+            assertEquals(1, completedOrders.size());
+            assertEquals(OrderStatus.COMPLETED, completedOrders.get(0).orderStatus());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldContinueProcessing_whenInvalidCommand() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-invalid", ".txt");
+        Files.writeString(testFile, "invalidCommand A 1\naddOrder B 1");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(1, allOrders.size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldContinueProcessing_whenInvalidProductId() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-invalid-product", ".txt");
+        Files.writeString(testFile, "addOrder A 999\naddOrder B 1");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(1, allOrders.size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldContinueProcessing_whenInvalidStatus() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-invalid-status", ".txt");
+        Files.writeString(testFile, "addOrder A 1\nsetStatus A INVALID_STATUS");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> processingOrders = shopService.getOrdersByOrderStatus(OrderStatus.PROCESSING);
+            assertEquals(1, processingOrders.size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldContinueProcessing_whenSetStatusForNonExistentOrder() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-nonexistent-order", ".txt");
+        Files.writeString(testFile, "setStatus NONEXISTENT COMPLETED\naddOrder A 1");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(1, allOrders.size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldIgnoreEmptyLines() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-empty-lines", ".txt");
+        Files.writeString(testFile, "addOrder A 1\n\naddOrder B 1\n   \n");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(2, allOrders.size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldHandleCompleteWorkflow() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-workflow", ".txt");
+        Files.writeString(testFile, 
+            "addOrder A 1\n" +
+            "addOrder B 1\n" +
+            "setStatus A COMPLETED\n" +
+            "setStatus B IN_DELIVERY\n" +
+            "printOrders"
+        );
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> completedOrders = shopService.getOrdersByOrderStatus(OrderStatus.COMPLETED);
+            List<Order> inDeliveryOrders = shopService.getOrdersByOrderStatus(OrderStatus.IN_DELIVERY);
+            
+            assertEquals(1, completedOrders.size());
+            assertEquals(1, inDeliveryOrders.size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldContinueProcessing_whenAddOrderHasNoProductIds() throws IOException {
+        //GIVEN
+        Path testFile = Files.createTempFile("test-insufficient-args", ".txt");
+        Files.writeString(testFile, "addOrder\naddOrder B 1");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(1, allOrders.size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
+    }
+
+    @Test
+    void processCommandsFromFile_shouldHandleMultipleProductsInOrder() throws IOException {
+        //GIVEN
+        // Add more products for this test
+        shopService.getProductRepo().addProduct(new Product("2", "Banane"));
+        shopService.getProductRepo().addProduct(new Product("3", "Kiwi"));
+        
+        Path testFile = Files.createTempFile("test-multiple-products", ".txt");
+        Files.writeString(testFile, "addOrder A 1 2 3");
+
+        try {
+            //WHEN
+            shopService.processCommandsFromFile(testFile);
+
+            //THEN
+            List<Order> allOrders = shopService.getOrders();
+            assertEquals(1, allOrders.size());
+            assertEquals(3, allOrders.get(0).products().size());
+        } finally {
+            Files.deleteIfExists(testFile);
+        }
     }
 
 }
